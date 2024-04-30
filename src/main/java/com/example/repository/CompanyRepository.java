c0481eb6781b42629a64f0cd9b0b164cf252a803
yurim0628 <dbfla0303@naver.com>
2024-04-30 13:23:35 +0900

    company repository 생성

---
+++ src/main/java/com/example/repository/CompanyRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import com.example.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Company findByCompanyName(String company);
+}
