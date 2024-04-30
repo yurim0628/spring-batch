fc693ab4a3ceb30366efcce42e11abf8ad801c98
yurim0628 <dbfla0303@naver.com>
2024-04-30 13:21:26 +0900

    DTO를 Entity로 변환하기 위한 ItemProcessor 설정 추가

--- src/main/java/com/example/config/ItemProcessorConfig.java
+++ src/main/java/com/example/config/ItemProcessorConfig.java
@@ -1,20 +1,28 @@
 package com.example.config;
 
 import com.example.dto.BranchDto;
+import com.example.dto.CompanyDto;
+import com.example.dto.EmployeeDto;
 import com.example.dto.MeetingRoomDto;
 import com.example.entity.Branch;
+import com.example.entity.Company;
+import com.example.entity.Employee;
 import com.example.entity.MeetingRoom;
 import com.example.repository.BranchRepository;
+import com.example.repository.CompanyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
 
 @Configuration
 @RequiredArgsConstructor
 public class ItemProcessorConfig {
 
     private final BranchRepository branchRepository;
+    private final CompanyRepository companyRepository;
 
     @Bean
     public ItemProcessor<BranchDto, Branch> branchesProcessor() {
@@ -46,6 +54,42 @@ public class ItemProcessorConfig {
             meetingRoom.setBranch(branch);
 
             return meetingRoom;
+        };
+    }
+
+    @Bean
+    public ItemProcessor<CompanyDto, Company> companiesProcessor() {
+        return companyDto -> Company.builder()
+                .companyName(companyDto.getCompanyName())
+                .business(companyDto.getBusiness())
+                .headCount(companyDto.getHeadCount())
+                .contractStartDate(LocalDate.parse(companyDto.getContractStartDate()))
+                .contractEndDate(LocalDate.parse(companyDto.getContractEndDate()))
+                .representativeName(companyDto.getRepresentativeName())
+                .representativeContact(companyDto.getRepresentativeContact())
+                .contractManagerName(companyDto.getContractManagerName())
+                .contractManagerContact(companyDto.getContractManagerContact())
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<EmployeeDto, Employee> employeesProcessor() {
+        return employeeDto -> {
+            Employee employee = Employee.builder()
+                    .loginId(employeeDto.getLoginId())
+                    .name(employeeDto.getName())
+                    .nickname(employeeDto.getNickname())
+                    .contact(employeeDto.getContact())
+                    .address(employeeDto.getAddress())
+                    .gender(employeeDto.getGender())
+                    .age(employeeDto.getAge())
+                    .email(employeeDto.getEmail())
+                    .build();
+
+            Company company = companyRepository.findByCompanyName(employeeDto.getCompany());
+            employee.setCompany(company);
+
+            return employee;
         };
     }
 }
